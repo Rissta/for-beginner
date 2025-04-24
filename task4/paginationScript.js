e874c24371177fc9b6ab047ed9bc97bf7ca09cc6
Rissta <laptevka22@oiate.ru>
2025-04-24 22:12:44 +0300

    Сделал пагинацию без адаптивной верствки

---
+++ task4/paginationScript.js
@@ -0,0 +1,56 @@
+function renderPage(page, count, paginationArea) {
+    const start = (page - 1) * count;
+    const end = page * count;
+    const currentData = data.slice(start, end);
+
+    let htmlContent = '';
+    currentData.forEach(item => {
+        htmlContent += `<div class="element-pagination"><p>${item}</p></div>`;
+    });
+    paginationArea.innerHTML = htmlContent;
+}
+
+function updateButtons(currentPage, totalPages, buttonArea, visibleButtons = 0) {
+    buttonArea.innerHTML = '';
+    buttonArea.innerHTML += '<button class="prev-button"> < </button>';
+    let startPage = Math.max(1, currentPage - Math.floor(visibleButtons / 2));
+    let endPage = Math.min(totalPages, startPage + visibleButtons - 1);
+    if (endPage - startPage + 1 < visibleButtons) {
+        startPage = Math.max(1, endPage - visibleButtons + 1);
+    }
+    for (let i = startPage; i <= endPage; i++) {
+        buttonArea.innerHTML += `<button class="number-button ${i === currentPage ? 'active' : ''}">${i}</button>`;
+    }
+    buttonArea.innerHTML += '<button class="next-button"> > </button>';
+}
+
+function setupPagination(data, count, paginationAreaName, buttonAreaName, visibleButtons = 5) {
+    const buttonArea = document.querySelector(buttonAreaName);
+    const paginationArea = document.querySelector(paginationAreaName);
+    const totalPages = Math.ceil(data.length / count);
+    let currentPage = 1;
+    function updateInterface() {
+        updateButtons(currentPage, totalPages, buttonArea, visibleButtons);
+        renderPage(currentPage, count, paginationArea);
+    }
+    buttonArea.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target.classList.contains('prev-button')) {
+            if (currentPage > 1) {
+                currentPage--;
+            }
+        } else if (target.classList.contains('next-button')) {
+            if (currentPage < totalPages) {
+                currentPage++;
+            }
+        } else if (target.classList.contains('number-button')) {
+            currentPage = parseInt(target.textContent);
+        }
+
+        updateInterface();
+    });
+    updateInterface();
+}
+
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+setupPagination(data, 33, '.pagination-container', '.pagination-button', 3);
